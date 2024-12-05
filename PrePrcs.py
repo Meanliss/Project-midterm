c6202bf836283fcdc53e8dc6953188ba925d417b
Likelipop <placetoworknottoplay@gmail.com>
2024-12-05 18:08:18 +0700

    hahaha

--- PrePrcs.py
+++ PrePrcs.py
@@ -3,88 +3,58 @@ import numpy as np
 from datetime import datetime
 from sklearn.preprocessing import LabelEncoder
 import joblib
+import re
 
 cvt_date = lambda date_obj: date_obj.strftime("%b-%d") 
 
-def explain_decision_tree_prediction(last_credit_pull_d, last_pymnt_d, term, sub_grade, prediction, model):
-    """
-    Explains the Decision Tree model's prediction (0: rejected, 1: accepted)
-    based on the input features.
-    """
+import shap
+def is_valid_zip_code(zip_code): 
+    """Checks if the provided zip code is valid.""" 
+    return bool(re.match(r'^\d{3}$', zip_code))
 
-    if prediction == 0:  # Loan Rejected
-        explanation = "The loan application was likely rejected due to the following factors:\n"
-
-        if last_credit_pull_d < model.tree_.threshold[0]:
-            explanation += "- **last_credit_pull_d:** The recent credit inquiry indicates potential financial instability.\n"
-        else:
-            explanation += "- **last_credit_pull_d:** The credit inquiry timing is favorable.\n"
-
-        if last_pymnt_d < model.tree_.threshold[1]:
-            explanation += "- **last_pymnt_d:** The recent last payment date might raise concerns about repayment capacity.\n"
-        else:
-            explanation += "- **last_pymnt_d:** The last payment date is not a major concern.\n"
-
-        if term < model.tree_.threshold[2]:
-            explanation += "- **term:** The shorter loan term could suggest higher risk.\n"
-        else:
-            explanation += "- **term:** The longer loan term is considered less risky.\n"
-
-        if sub_grade < model.tree_.threshold[3]:
-            explanation += "- **sub_grade:** The lower sub_grade indicates a higher risk profile.\n"
-        else:
-            explanation += "- **sub_grade:** The sub_grade is acceptable.\n"
-
-        return explanation
-
-    else:  # Loan Accepted
-        return "The loan application was likely accepted based on the provided features."
-
-def handle_data(input_data):
-    print("flag")
-    #unpacking
-    last_credit_pull_d, last_pymnt_d, term, sub_grade = input_data
-    loan_df = pd.read_csv("Data/loan_df.csv")
-
-    # Làm tương tự cho các cột khác trong binary_cols
-
-    # Convert inputs to the same format as loan.csv
-    last_credit_pull_d = cvt_date(last_credit_pull_d)
-    last_pymnt_d       = cvt_date(last_pymnt_d)
+def explain_xgb_prediction(model, input_data, top_n=3):
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(input_data)
+    shap.initjs()
+    shap.force_plot(explainer.expected_value, shap_values, input_data)
     
+    # Lấy top N đặc trưng quan trọng nhất
+    importance_df = pd.DataFrame(list(zip(input_data.columns, shap_values[0])), columns=['feature', 'shap_value'])
+    importance_df['abs_shap_value'] = importance_df['shap_value'].abs()
+    top_features = importance_df.sort_values(by='abs_shap_value', ascending=False).head(top_n)
+    return top_features
+
+
+def handle_data(new_data):
+    print("flag")
     # Create a DataFrame for the input
-    input_data = pd.DataFrame({
-        'last_credit_pull_d': [last_credit_pull_d],
-        'last_pymnt_d': [last_pymnt_d],
-        'term': [term],
-        'sub_grade': [sub_grade]
-    })
+    input_data = pd.DataFrame([new_data])
 
-    #label data
-    label_encoder = LabelEncoder()
+    #sync
+    input_data.select_dtypes(include=['datetime64[ns]']).apply(cvt_date)
+    input_data['emp_length'] = input_data['emp_length'].str.strip().str[0].astype(int)
 
-    #add the data point in the bottoms of the df
-    filter = 'last_credit_pull_d last_pymnt_d term sub_grade'.split(' ')
-    loan_df = loan_df[filter]
-    loan_df= pd.concat([loan_df,input_data], ignore_index= True) 
+    
+    label_encoder = joblib.load("Transform/label_encoder.joblib")
+    freq_encoding = joblib.load("Transform/freq_encoding.joblib")
 
+    binary_cols = ['term', 'sub_grade', 'home_ownership', 'verification_status', 'purpose', 'title', 'zip_code', 'earliest_cr_line', 'last_pymnt_d', 'last_credit_pull_d', 'emp_title'] 
+    for col in binary_cols:
+        # Nếu giá trị mới không có trong classes_ của LabelEncoder, hãy gán nó là -1 (hoặc giá trị khác tùy chỉnh)
+        if input_data[col][0] not in label_encoder.classes_:
+            input_data[col] = -1 
+        else:
+            input_data[col] = label_encoder.transform(input_data[col])
 
-    for col in input_data:
-        loan_df[col] = label_encoder.fit_transform(loan_df[col])
-        input_data[col] = label_encoder.transform(input_data[col])
+    for col in binary_cols:
+        # Nếu giá trị mới không có trong frequency_encoding, hãy gán nó là 0 (hoặc giá trị khác tùy chỉnh)
+        if input_data[col][0] not in freq_encoding:
+            input_data[col] = 0 
+        else:
+            input_data[col] = freq_encoding[input_data[col][0]]
 
-    for col in input_data:
-        freq_encoding = loan_df[col].value_counts().to_dict()
-        freq_encoding = loan_df[col].map(freq_encoding)
-        input_data[col] = input_data[col].map(freq_encoding).fillna(0) 
-
-    mapping_term = {
-        '36 months' : 26678,
-        '60 months' : 9047,
-        'NaN' : 1091
-    }
-
-    input_data['term'] = mapping_term[term]
+    for key in input_data:
+        print(f" {key}  : {input_data[key]}")
     return input_data
 
 # Kiểm tra và thêm nhãn mới nếu cần
--- app.py
+++ app.py
@@ -8,14 +8,42 @@ from sklearn.preprocessing import LabelEncoder
 import shap
 import matplotlib.pyplot as plt
 import os
-from PrePrcs import handle_data, explain_decision_tree_prediction
+from PrePrcs import handle_data, explain_xgb_prediction, is_valid_zip_code
+import xgboost as xgb
+from xgboost import XGBClassifier
+#=========================================load explaination for eachs features ===================================
+FEATURE_EXPLANATIONS = {
+    "last_pymnt_d": "The date of your last payment. If your last payment was a long time ago, this can negatively impact your credit evaluation.",
+    "last_credit_pull_d": "The date when your credit information was last checked. If this information is outdated, it may raise concerns about your current financial status.",
+    "sub_grade": "This indicates the risk level of the loan. Loans with a lower sub_grade are often seen as riskier and have higher interest rates.",
+    "zip_code": "Your postal code. This can be related to the economic status of the area where you live.",
+    "int_rate": "The interest rate of the loan. A higher interest rate can increase the overall cost of the loan and impact your ability to repay.",
+    "revol_bal": "Your revolving balance. A high revolving balance can indicate that you are using a lot of credit and may have difficulty repaying.",
+    "purpose": "The purpose of the loan. Certain loan purposes may be seen as riskier and affect the lending decision.",
+    "earliest_cr_line": "The date when you first opened a credit account. A longer credit history is often viewed positively.",
+    "revol_util": "The revolving credit utilization rate. A high utilization rate can indicate higher credit risk.",
+    "term": "The term of the loan. A longer term can mean more interest paid over time.",
+    "dti": "The debt-to-income ratio. A higher ratio can indicate that you have a lot of debt relative to your income, which may lower your ability to repay.",
+    "last_pymnt_amnt": "The amount of your last payment. A low last payment amount can negatively affect your credit evaluation.",
+    "total_rec_int": "The total received interest. This can indicate your creditworthiness.",
+    "emp_title": "Your job title. Certain job titles may be viewed as higher risk by lenders.",
+    "title": "The title of your loan. An unclear or concerning title can affect the lending decision.",
+    "total_pymnt": "The total payment amount. A low total payment amount can increase lending risk.",
+    "loan_amnt": "The amount of the loan. A higher loan amount can increase credit risk.",
+    "total_rec_prncp": "The total principal received. This can indicate your creditworthiness.",
+    "annual_inc": "Your annual income. A lower income can negatively impact your ability to repay.",
+    "total_acc": "The total number of accounts. A larger number of accounts can indicate more credit experience.",
+    "funded_amnt": "The funded amount. A higher funded amount can increase lending risk.",
+    "home_ownership": "Your home ownership status. Owning a home is often seen as a positive financial signal.",
+    "open_acc": "The number of open accounts. A larger number of open accounts can indicate good credit management experience.",
+    "verification_status": "The verification status. The verification status of your credit can impact the lending decision.",
+    "emp_length": "The length of employment. A longer employment history is often viewed positively regarding financial stability."
+}
 
-    
-#load model
-dt_path = "Model/dt_model.pkl"
-# lr_path = "Model\logistic_model.pkl"
-# nn_path =  "Model\neural_network_model.pkl"
-dt_model = joblib.load(dt_path)
+#============================================load model==========================================================
+xgb_path = "Model/xgb_model.json"
+xgb_model = XGBClassifier() 
+xgb_model.load_model(xgb_path)
 
 today = datetime.date.today()
 
@@ -27,57 +55,143 @@ st.markdown(" predict loan application acceptance or rejection based on customer
 st.header('Customer Details')
 
 #=============================================Input fields========================================================
-last_credit_pull_d = st.date_input('Last Credit Pull Date', 
-                                   datetime.date(2024, 1, 1),
-                                   max_value=today)
-last_pymnt_d = st.date_input('Last Payment Date', 
-                             datetime.date(2024, 1, 1),
-                             max_value=today)
-term = st.selectbox('Term', 
-                    ['36 months', '60 months'])
-sub_grade = st.selectbox('Sub Grade', 
-                         [f'{chr(i)}{j}' for i in range(ord('A'), ord('H')) for j in range(1, 6)])
 
-#convert data into a type that fit the model
-input_data = handle_data([last_credit_pull_d,
-                          last_pymnt_d,
-                          term,
-                          sub_grade])
+# Chia giao diện thành 2 cột
+col1, col2 = st.columns(2)
+
+# Cột 1: Ngày tháng và các thuộc tính phân loại
+with col1:
+    st.header("Thông tin phân loại")
+    last_pymnt_d = st.date_input("Last Payment Date", value=datetime.date.today(), max_value=today)
+    last_credit_pull_d = st.date_input("Last Credit Pull Date", value=datetime.date.today(), max_value=today)
+    earliest_cr_line = st.date_input("Earliest Credit Line", value=datetime.date.today(), max_value=today)
+    sub_grade = st.selectbox('Sub Grade', 
+                         [f'{chr(i)}{j}' for i in range(ord('A'), ord('H')) for j in range(1, 6)])
+    zip_code = st.text_input("ZIP Code (first 3 digits)")
+    purpose = st.selectbox("Purpose", ['debt_consolidation', 'credit_card', 'other', 
+                                       'home_improvement','major_purchase', 'small_business', 
+                                       'car', 'wedding', 'medical', 
+                                       'moving', 'vacation', 'house',
+                                        'educational', 'renewable_energy'])
+    emp_title = st.text_input("Employment Title")
+    title = st.text_input("Loan Title")
+    home_ownership = st.selectbox("Home Ownership", ["OWN", "MORTGAGE", "RENT", "OTHER"])
+    verification_status = st.selectbox("Verification Status", ["Verified", "Source Verified", "Not Verified"])
+    emp_length = st.selectbox("Employment Length", [f"{i} years" for i in range(1, 11)] + ["< 1 year"])
+
+# Cột 2: Các thuộc tính số
+with col2:
+    st.header("Thông tin số liệu")
+    int_rate = st.number_input("Interest Rate (%)", min_value=0.0, max_value=100.0, step=0.1)
+    revol_bal = st.number_input("Revolving Balance", min_value=0.0, step=100.0)
+    revol_util = st.number_input("Revolving Utilization Rate (%)", min_value=0.0, max_value=100.0, step=0.1)
+    term = st.selectbox("Loan Term", ["36 months", "60 months"])
+    dti = st.number_input("Debt-to-Income Ratio", min_value=0.0, step=0.1)
+    last_pymnt_amnt = st.number_input("Last Payment Amount", min_value=0.0, step=100.0)
+    total_rec_int = st.number_input("Total Received Interest", min_value=0.0, step=100.0)
+    total_pymnt = st.number_input("Total Payment", min_value=0.0, step=100.0)
+    loan_amnt = st.number_input("Loan Amount", min_value=0.0, step=100.0)
+    total_rec_prncp = st.number_input("Total Received Principal", min_value=0.0, step=100.0)
+    annual_inc = st.number_input("Annual Income", min_value=0.0, step=1000.0)
+    total_acc = st.number_input("Total Accounts", min_value=0, step=1)
+    funded_amnt = st.number_input("Funded Amount", min_value=0.0, step=100.0)
+    open_acc = st.number_input("Open Accounts", min_value=0, step=1)
+
+# Hiển thị kết quả đầu ra
 
 #=============================================logging============================================================
-print(f"{last_credit_pull_d} : {type(last_credit_pull_d)}")
-print(f"{last_pymnt_d}: {type(last_pymnt_d)}")
-print(f"{term}: {type(term)}")
-print(f"{sub_grade} : {type(sub_grade)}")
-filter = 'last_credit_pull_d last_pymnt_d term sub_grade'.split(' ')
-print(input_data[filter])
+result = {
+        "last_pymnt_d": last_pymnt_d,
+        "last_credit_pull_d": last_credit_pull_d,
+        "sub_grade": sub_grade,
+        "zip_code": zip_code,
+        "int_rate": int_rate,
+        "revol_bal": revol_bal,
+        "purpose": purpose,
+        "earliest_cr_line": earliest_cr_line,
+        "revol_util": revol_util,
+        "term": term,
+        "dti": dti,
+        "last_pymnt_amnt": last_pymnt_amnt,
+        "total_rec_int": total_rec_int,
+        "emp_title": emp_title,
+        "title": title,
+        "total_pymnt": total_pymnt,
+        "loan_amnt": loan_amnt,
+        "total_rec_prncp": total_rec_prncp,
+        "annual_inc": annual_inc,
+        "total_acc": total_acc,
+        "funded_amnt": funded_amnt,
+        "home_ownership": home_ownership,
+        "open_acc": open_acc,
+        "verification_status": verification_status,
+        "emp_length": emp_length,
+    }
+for key in result:
+    print(f"{key} : {type(result[key])} " )
+
+print("=========== key - values =======")
+
+for key in result:
+    print(f"{key} : {result[key] } " )
+
+#==================================Data processing================================================================
+# convert data into a type that fit the model
+input_data = handle_data(result)
 
 #==================================Predict and display the result=================================================
-if st.button('Predict'):
-    print(f" predict in : {input_data.values.reshape(1,-1)}")
-    prediction = dt_model.predict(input_data.values.reshape(1,-1))
-    print(prediction)
-    st.success(f'{"Approved " if prediction[0] == 1 else "Not Approved"}')
+if not zip_code or not emp_title or not title: 
+    st.error("Please fill out all required fields: ZIP Code, Employment Title, and Loan Title.") 
+elif not is_valid_zip_code(zip_code): 
+    st.error("Please enter a valid ZIP Code (first 3 digits onlu).")
+else:
 
-    explain = explain_decision_tree_prediction(*input_data.iloc[0],prediction[0],dt_model)
-    
-    # Get SHAP values for the input data
-    explainer = shap.TreeExplainer(dt_model)
-    shap_values = explainer.shap_values(input_data)
+    if st.button('Predict'):
+        print(f" predict in : {input_data.values.reshape(1,-1)}")
+        prediction = xgb_model.predict(input_data)
+        print(prediction)
+        if prediction[0] == 1:
+            st.success('Approved')
+        else:
+            st.error('Not Approved')
+            
+            # Giải thích quyết định từ chối
+            top_features = explain_xgb_prediction(xgb_model, input_data)
+            st.markdown("### Explanation for Rejection (top three features that contributes to this rejection)")
+            for idx, row in top_features.iterrows():
+                feature_name = row['feature']
+                explanation = FEATURE_EXPLANATIONS.get(feature_name, "There is no features for this explaination.")
+                st.markdown(f"**{idx + 1}. {feature_name}**: {explanation}")
+                st.markdown(f"This features effects {row['shap_value']:.2f}.")
 
-    # Generate the SHAP force plot
 
-    print(shap_values.shape)
-    print(input_data.shape)
-    print()
 
-    shap.initjs()
-    shap.force_plot(explainer.expected_value[0],  # Giá trị kỳ vọng cho lớp đầu tiên
-                    shap_values[0, :, 1].reshape((1, 4)),  # Giá trị SHAP cho lớp đầu tiên
-                    input_data.iloc[0].values.reshape((1, 4)),  # Dữ liệu gốc (dòng đầu tiên của DataFrame)
-                    matplotlib=True,
-                    show = True)
+# if st.button('Predict'):
+#     print(f" predict in : {input_data.values.reshape(1,-1)}")
+#     prediction = xgb_model.predict(input_data)
+#     print(prediction)
+#     if prediction[0] == 1:
+#         st.success('Approved')
+#     else:
+#         st.error('Not Approved')
+        
+#         # Giải thích quyết định từ chối
+#         top_features = explain_xgb_prediction(xgb_model, input_data)
+#         st.markdown("### Explanation for Rejection")
+#         for idx, row in top_features.iterrows():
+#             st.markdown(f"**{idx + 1}. {row['feature']}** contributed to the rejection with an impact of {row['shap_value']:.2f}.")
 
-    plt.tight_layout()
-    st.pyplot(plt)
-    st.success(explain)
+
+
+# if st.button('Predict'):
+#     print(f" predict in : {input_data.values.reshape(1,-1)}")
+#     #dmatrix_data = xgb.DMatrix(input_data.values, feature_names=input_data.columns)
+#     prediction = xgb_model.predict(input_data)
+#     print(prediction)
+#     st.success(f'{"Approved " if prediction[0] == 1 else "Not Approved"}')
+
+#     # Lời giải thích
+
+#     # Hiển thị lời giải thích
+#     st.markdown("### Explanation")
+#     st.markdown(explanation)
